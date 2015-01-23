bc9d3a393e9019517226d91fce71194b8eca40be
Marc Roberts <marc@neutroncreations.com>
2015-01-23 11:44:29 +0000

    added stream stuff

--- index.js
+++ index.js
@@ -1,13 +1,64 @@
-var express = require('express');
-var app = express();
+var app = require('express')(),
+    dotenv = require('dotenv'),
+    server = require('http').Server(app),
+    io = require('socket.io')(server),
+    xt = require('keyword-extractor'),
+    franc = require('franc'),
+    twitter = require('./twitter-stream'),
+    stream,
+    clients = 0;
 
-app.set('port', (process.env.PORT || 5000));
-app.use(express.static(__dirname + '/public'));
+dotenv.load();
+
+stream = new twitter({
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  token: process.env.TWITTER_TOKEN,
+  token_secret: process.env.TWITTER_TOKEN_SECRET
+});
+
+io.set('origins', '*');
+
+server.listen(process.env.PORT || 5000);
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  response.status(403).send('VERBOTEN');
 });
 
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'));
+io.on('connect', function(socket) {
+  clients++;
+  console.log('CLIENT CONNECTED - COUNT=' + clients);
+
+  socket.on('disconnect', function() {
+    clients--;
+    console.log('CLIENT DISCONNECTED - COUNT=' + clients);
+  });
 });
+
+
+function exceptions(word){
+  if (word.match(/^https?:/)) return false;
+  if (word.match(/^@/)) return false;
+
+  return true;
+}
+
+
+stream.on('tweet', function(tweet) {
+  // ignore retwets
+  if (tweet.retweeted_status || tweet.text.match(/^RT/)) return;
+
+  // only english for now
+  if (franc(tweet.text) != 'eng') return;
+
+  // parse that tweet, extract words
+  words = xt.extract(tweet.text,{
+    language:"english",
+    remove_digits: true,
+    return_changed_case:true
+  }).filter(exceptions);
+
+  if (words.length > 0) io.emit('tweet', words);
+});
+
+stream.connect();
